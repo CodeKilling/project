f904b8c92258c0ad863908762d8d4fd3ba4ef40c
parksunyoung <iss002@naver.com>
2021-08-26 16:10:40 +0900

    project 생성

---
+++ src/main/MainClass.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+		while(true) {
+			System.out.println("1.박선영 2.박상진 3.문지은 4.김형석 5.김여진");
+			num = sc.nextInt();
+			switch(num) {
+			case 1: break;
+			case 2: break;
+			case 3: break;
+			case 4: break;
+			case 5: break;
+			}
+		}
+	}
+}
