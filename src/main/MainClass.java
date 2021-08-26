f26475f29c0bbbcd7434d3f763f1cd26a782a24a
parksunyoung <iss002@naver.com>
2021-08-26 16:39:18 +0900

    psy:: test func comp

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,15 +2,19 @@ package main;
 
 import java.util.Scanner;
 
+import psy.TestPsy;
+
 public class MainClass {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = 0;
+		TestPsy tPsy = new TestPsy();
 		while(true) {
 			System.out.println("1.박선영 2.박상진 3.문지은 4.김형석 5.김여진");
 			num = sc.nextInt();
 			switch(num) {
-			case 1: break;
+			case 1:
+				tPsy.display(); break;
 			case 2: break;
 			case 3: break;
 			case 4: break;
