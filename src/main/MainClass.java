3cffaaba293d6468095f21fbb397f995758d8068
Fiction2st <babu6322@naver.com>
2021-08-26 17:07:05 +0900

    김형석 작성 완료

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,10 +2,13 @@ package main;
 
 import java.util.Scanner;
 
+import hyungseok.hyungseok_method;
+
 public class MainClass {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = 0;
+		hyungseok_method hs = new hyungseok_method();
 		while(true) {
 			System.out.println("1.박선영 2.박상진 3.문지은 4.김형석 5.김여진");
 			num = sc.nextInt();
@@ -13,7 +16,9 @@ public class MainClass {
 			case 1: break;
 			case 2: break;
 			case 3: break;
-			case 4: break;
+			case 4: 
+				hs.hs_print();
+				break;
 			case 5: break;
 			}
 		}
